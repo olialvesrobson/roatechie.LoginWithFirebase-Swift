a54cefba61d673ea2b0c0641c06ef294832bb438
Robson Oliveira Alves <olialves.robson@gmail.com>
2020-11-05 15:54:25 +1100

    Initial Commit

---
+++ LoginWithFirebase/LoginWithFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithFirebaseApp.swift
+//  LoginWithFirebase
+//
+//  Created by Robson Oliveira Alves on 5/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LoginWithFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
